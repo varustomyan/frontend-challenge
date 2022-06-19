1fd7b781fcf4b5156d9b0e5a47b6716794b2b616
varustomyan <v.arustomyan@icloud.com>
2022-06-19 20:25:50 +0300

    add dotenv

--- src/components/API/RestApi.js
+++ src/components/API/RestApi.js
@@ -4,7 +4,7 @@ export default class RestApi {
       `https://api.thecatapi.com/v1/images/search?mime_types=jpg,png&page=${page}&limit=15&order=Asc&size=med`,
       {
         headers: {
-          "x-api-key": "66b3ec38-2ef5-4770-8cf5-af389b5ac452",
+          "x-api-key": process.env.REACT_APP_API_KEY,
           "Content-Type": "application/json",
         },
         redirect: "follow",
